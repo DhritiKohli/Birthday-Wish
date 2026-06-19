9a008cc243ea60b717f98d95bc46dc995221fb21
Dhriti Kohli <dhritikohli@gmail.com>
2026-06-18 20:11:00 -0500

    Candle Animation + Blow Feature

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,6 +1,8 @@
 const viewLetterBtn = document.getElementById("letterButton");
 const letter = document.getElementById("letterContent");
 const exitLetter = document.getElementById("letterExit");
+const candleLightVideo = document.getElementById("candleLightVideo");
+const blowCandleBtn = document.getElementById("blowCandleBtn");
 // Letter Functionality
 
 viewLetterBtn.addEventListener("click", () => {
@@ -9,4 +11,38 @@ viewLetterBtn.addEventListener("click", () => {
 
 exitLetter.addEventListener("click", ()=>{
     letter.style.display= 'none';
+})
+
+if (candleLightVideo) {
+    const keepPlaying = () => {
+        const playAttempt = candleLightVideo.play();
+
+        if (playAttempt && typeof playAttempt.catch === "function") {
+            playAttempt.catch(() => {
+                // Autoplay can still be blocked in some browsers until the first interaction.
+            });
+        }
+    };
+
+    candleLightVideo.addEventListener("pause", () => {
+        keepPlaying();
+    });
+
+    candleLightVideo.addEventListener("ended", () => {
+        keepPlaying();
+    });
+
+    keepPlaying();
+}
+
+blowCandleBtn.addEventListener("click",()=>
+{
+    if (candleLightVideo) {
+        const blowedCake = document.createElement("img");
+        blowedCake.src="../assets/img/CakeBlown.png";
+        blowedCake.id = "candleLightVideo";
+        blowedCake.classList.add("animated-object");
+        
+        candleLightVideo.replaceWith(blowedCake);
+    }
 })
